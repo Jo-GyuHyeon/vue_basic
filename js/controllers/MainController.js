3cd2befa472ef3e329111a4fd2ae3fe1423f0ed0
JoGyuHyeion <kuhyun1993@naver.com>
2018-08-19 00:59:18 +0900

    add input tag reset button(default none)

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,10 @@
+import FormView from '../views/FormView.js';
+
 const tag = '[MainControllers]'
 
 export default{
   init(){
     console.log(tag, 'init()');
+    FormView.setup(document.querySelector('form'));
   }
 }
