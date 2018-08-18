f2269ec6094fe168bd553b46cfd904e7c7657e52
JoGyuHyeion <kuhyun1993@naver.com>
2018-08-19 01:04:00 +0900

    add keyup => resetButton display

--- js/views/FormView.js
+++ js/views/FormView.js
@@ -2,17 +2,26 @@ import View from './View.js';
 
 const tag = '[FormView]';
 
-const FormView = Object.create(View)
+const FormView = Object.create(View);
 
 FormView.setup = function (el) {
-  this.init(el)
-  this.inputEL = el.querySelector('[type=text]')
+  this.init(el);
+  this.inputEL = el.querySelector('[type=text]');
   this.resetEl = el.querySelector('[type=reset]');
-  this.showResetBtn(false)
+  this.bindEvents();
+  this.showResetBtn(false);
 }
 
 FormView.showResetBtn = function (show = true) {
-  this.resetEl.style.display = show ? 'block' : 'none'
+  this.resetEl.style.display = show ? 'block' : 'none';
 }
 
-export default FormView
+FormView.bindEvents = function () {
+  this.inputEL.addEventListener('keyup', e => this.onKeyup(e));
+}
+
+FormView.onKeyup = function(){
+  this.showResetBtn(this.inputEL.value.length);
+}
+
+export default FormView;
