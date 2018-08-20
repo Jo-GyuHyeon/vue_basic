bc1a5978fad40785db3963506a17b5cb883d288b
Jo-GyuHyeon <kuhyun1993@naver.com>
2018-08-20 17:40:30 +0900

    added:  - enter key press submit  - reset button onClick

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -5,6 +5,16 @@ const tag = '[MainControllers]'
 export default{
   init(){
     console.log(tag, 'init()');
-    FormView.setup(document.querySelector('form'));
+    FormView.setup(document.querySelector('form'))
+      .on('@submit',e => this.onSubmit(e.detail.input))
+      .on('@reset',e => this.onRest(e.detail.input))
+  },
+
+  onSubmit(input){
+    console.log(tag,'onSubmit()',input);
+  },
+
+  onRest(input){
+    console.log(tag,'onRest()',input);
   }
 }
--- js/views/FormView.js
+++ js/views/FormView.js
@@ -7,21 +7,31 @@ const FormView = Object.create(View);
 FormView.setup = function (el) {
   this.init(el);
   this.inputEL = el.querySelector('[type=text]');
-  this.resetEl = el.querySelector('[type=reset]');
-  this.bindEvents();
+  this.resetEL = el.querySelector('[type=reset]');
   this.showResetBtn(false);
+  this.bindEvents();
+  return this
 }
 
 FormView.showResetBtn = function (show = true) {
-  this.resetEl.style.display = show ? 'block' : 'none';
+  this.resetEL.style.display = show ? 'block' : 'none';
 }
 
 FormView.bindEvents = function () {
+  this.on('submit', e => e.preventDefault())
   this.inputEL.addEventListener('keyup', e => this.onKeyup(e));
+  this.resetEL.addEventListener('click', e => this.onClick(e));
 }
 
-FormView.onKeyup = function(){
+FormView.onKeyup = function (e) {
+  const enter = 13;
   this.showResetBtn(this.inputEL.value.length);
+  if (e.keyCode !== enter) return
+  this.emit('@submit', { input: this.inputEL.value })
+}
+
+FormView.onClick = function (e) {
+  this.emit('@reset', { input: '' })
 }
 
 export default FormView;
