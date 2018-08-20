fe7165f974d00e0f3dfae2ccf35d4a43def684c6
Jo-GyuHyeon <kuhyun1993@naver.com>
2018-08-20 19:00:20 +0900

    added: resultView  - input search then result

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,20 +1,39 @@
 import FormView from '../views/FormView.js';
+import ResultView from '../views/ResultView.js';
+
+import SearchModel from '../models/SearchModel.js';
 
 const tag = '[MainControllers]'
 
-export default{
-  init(){
+export default {
+  init() {
     console.log(tag, 'init()');
     FormView.setup(document.querySelector('form'))
-      .on('@submit',e => this.onSubmit(e.detail.input))
-      .on('@reset',e => this.onRestFrom())
+      .on('@submit', e => this.onSubmit(e.detail.input))
+      .on('@reset', e => this.onRestFrom())
+
+    ResultView.setup(document.querySelector('#search-result'))
+
   },
 
-  onSubmit(input){
-    console.log(tag,'onSubmit()',input);
+  serarch(query) {
+    console.log(tag, 'search()', query)
+    SearchModel.list(query).then(data => {
+      this.onSearchResult(data)
+    })
+   
   },
 
-  onRestFrom(){
-    console.log(tag,'onRest()');
+  onSubmit(input) {
+    console.log(tag, 'onSubmit()', input);
+    this.serarch(input)
+  },
+
+  onRestFrom() {
+    console.log(tag, 'onRest()');
+  },
+
+  onSearchResult(data){
+    ResultView.render(data)
   }
 }
