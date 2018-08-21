45ed695db600796ba4f2e060cbc26782c0485e1b
Jo-GyuHyeon <kuhyun1993@naver.com>
2018-08-22 03:42:18 +0900

    added:  - tab 기능추가

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,51 +1,55 @@
-import FormView from '../views/FormView.js'
-import ResultView from '../views/ResultView.js'
-import TabView from '../views/TabView.js'
+import FormView from "../views/FormView.js";
+import ResultView from "../views/ResultView.js";
+import TabView from "../views/TabView.js";
 
-import SearchModel from '../models/SearchModel.js'
+import SearchModel from "../models/SearchModel.js";
 
-const tag = '[MainControllers]'
+const tag = "[MainControllers]";
 
 export default {
   init() {
-    console.log(tag, 'init()');
-    FormView.setup(document.querySelector('form'))
-      .on('@submit', e => this.onSubmit(e.detail.input))
-      .on('@reset', e => this.onRestFrom())
+    console.log(tag, "init()");
+    FormView.setup(document.querySelector("form"))
+      .on("@submit", e => this.onSubmit(e.detail.input))
+      .on("@reset", e => this.onRestFrom());
 
-    TabView.setup(document.querySelector('#tabs'))
+    TabView.setup(document.querySelector("#tabs"))
+    .on("@change", e => this.onChangeTab(e.detail.tabName));
 
-    ResultView.setup(document.querySelector('#search-result'))
+    ResultView.setup(document.querySelector("#search-result"));
 
-    this.selectedTab = '추천 검색어'
-    this.renderView()
+    this.selectedTab = "추천 검색어";
+    this.renderView();
   },
 
-  renderView(){
-    console.log(tag, 'renderView()');
+  renderView() {
+    console.log(tag, "renderView()");
     TabView.setActiveTab(this.selectedTab);
     ResultView.hide();
   },
 
   serarch(query) {
-    console.log(tag, 'search()', query)
+    console.log(tag, "search()", query);
     SearchModel.list(query).then(data => {
-      this.onSearchResult(data)
-    })
-   
+      this.onSearchResult(data);
+    });
   },
 
   onSubmit(input) {
-    console.log(tag, 'onSubmit()', input);
-    this.serarch(input)
+    console.log(tag, "onSubmit()", input);
+    this.serarch(input);
   },
 
   onRestFrom() {
-    console.log(tag, 'onRest()');
-    ResultView.hide()
+    console.log(tag, "onRest()");
+    ResultView.hide();
   },
 
-  onSearchResult(data){
-    ResultView.render(data)
+  onSearchResult(data) {
+    ResultView.render(data);
+  },
+
+  onChangeTab(tabName) {
+    debugger
   }
-}
+};
--- js/views/FormView.js
+++ js/views/FormView.js
@@ -1,39 +1,39 @@
-import View from './View.js';
+import View from "./View.js";
 
-const tag = '[FormView]';
+const tag = "[FormView]";
 
 const FormView = Object.create(View);
 
-FormView.setup = function (el) {
+FormView.setup = function(el) {
   this.init(el);
-  this.inputEL = el.querySelector('[type=text]');
-  this.resetEL = el.querySelector('[type=reset]');
+  this.inputEL = el.querySelector("[type=text]");
+  this.resetEL = el.querySelector("[type=reset]");
   this.showResetBtn(false);
   this.bindEvents();
-  return this
-}
+  return this;
+};
 
-FormView.showResetBtn = function (show = true) {
-  this.resetEL.style.display = show ? 'block' : 'none';
-}
+FormView.showResetBtn = function(show = true) {
+  this.resetEL.style.display = show ? "block" : "none";
+};
 
-FormView.bindEvents = function () {
-  this.on('submit', e => e.preventDefault())
-  this.inputEL.addEventListener('keyup', e => this.onKeyup(e));
-  this.resetEL.addEventListener('click', e => this.onClickReset(e));
-}
+FormView.bindEvents = function() {
+  this.on("submit", e => e.preventDefault());
+  this.inputEL.addEventListener("keyup", e => this.onKeyup(e));
+  this.resetEL.addEventListener("click", e => this.onClickReset(e));
+};
 
-FormView.onKeyup = function (e) {
+FormView.onKeyup = function(e) {
   const enter = 13;
   this.showResetBtn(this.inputEL.value.length);
-  if(!this.inputEL.value.length) this.emit('@reset')
-  if (e.keyCode !== enter) return
-  this.emit('@submit', { input: this.inputEL.value })
-}
+  if (!this.inputEL.value.length) this.emit("@reset");
+  if (e.keyCode !== enter) return;
+  this.emit("@submit", { input: this.inputEL.value });
+};
 
-FormView.onClickReset = function (e) {
-  this.emit('@reset')
-  this.showResetBtn(false)
-}
+FormView.onClickReset = function(e) {
+  this.emit("@reset");
+  this.showResetBtn(false);
+};
 
 export default FormView;
