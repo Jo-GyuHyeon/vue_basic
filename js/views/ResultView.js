e5d4bf385eff84de26b5d0ec489b8bf6eab1f5d7
Jo-GyuHyeon <kuhyun1993@naver.com>
2018-08-21 11:22:45 +0900

    added:  - input keyword then show search result view

--- js/views/ResultView.js
+++ js/views/ResultView.js
@@ -1,21 +1,32 @@
 import View from './View.js'
 
-const tag ='[ResultView]'
+const tag = '[ResultView]'
 
 const ResultView = Object.create(View)
 
-ResultView.setup = function (el){
+ResultView.setup = function (el) {
     this.init(el)
 }
 
-ResultView.render = function (data = []){
-    console.log(tag,'render()',data)
-    
-    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data): '검색 경로가 없습니다.'
+ResultView.render = function (data = []) {
+    console.log(tag, 'render()', data)
+
+    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : '검색 경로가 없습니다.'
 }
 
 ResultView.getSearchResultsHtml = function (data) {
-    debugger
+    
+    return data.reduce((html, item) => {
+        html += this.getSearchItemHtml(item)
+        return html;
+    }, '<ul>') + '</ul>'
+}
+
+ResultView.getSearchItemHtml = function(item) {
+    return `<li>
+    <img src=${item.image} >
+    <p>${item.name}</p>
+    </li>`
 }
 
 export default ResultView
