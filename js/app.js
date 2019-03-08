8017dd4be8cf6646e8da73e91d56223a3b39b68d
Jo-GyuHyeon <kuhyun1993@naver.com>
2019-03-08 18:47:35 +0900

    added: 검색 결과

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
+import SearchModel from '../models/SearchModel.js';
+
 new Vue({
   el: '#app',
   data: {
-    query: '' //입력 부분을 맡고 있다
+    query: '', //입력 부분을 맡고 있다
+    submitted: false,
+    searchResult: []
   },
   methods: {
     onSubmit(e) {
-      e.preventDefault;
+      this.search();
     },
     onkeyup() {
       if (!this.query.length) this.onReset();
     },
     onReset() {
+      this.resetForm();
+    },
+    search() {
+      SearchModel.list().then(data => {
+        this.submitted = true;
+        this.searchResult = data;
+      });
+    },
+    resetForm() {
       this.query = '';
       debugger;
     }
